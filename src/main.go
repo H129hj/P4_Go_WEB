8c1cb299f13db2165dce7fb0690e8f06b5a5e2d7
Shakil <khaldishakil@gmail.com>
2025-11-24 08:26:04 +0100

    feat: page du jeu et css

--- src/main.go
+++ src/main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"html/template"
+	"net/http"
 	"os"
 )
 
 func main() {
+	type GamePage struct {
+		Grille [6][7]string
+		Tour   int
+		Joueur1 string
+		Joueur2 string
+		JetonCouleur string
+	}
+
+	type GameData struct {
+		Joueur1     string
+		Joueur2     string
+		JetonCouleur string
+	}
+
+	var currentGame GameData
+
 	listTemplate, errTemp := template.ParseGlob("./templates/*.html")
 	if errTemp != nil {
 		fmt.Println(errTemp.Error())
@@ -35,15 +51,38 @@ func main() {
 	})
 
 	http.HandleFunc("/game/init/traitement", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/game/play", http.StatusSeeOther)
-	})
+    if r.Method != http.MethodPost {
+        http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+        return
+    }
+
+    // Récupération des valeurs du formulaire
+    j1 := r.FormValue("name")
+    j2 := r.FormValue("name2")
+    jetonCouleur := r.FormValue("jetoncolor")
+
+    // Stockage dans la variable globale
+    currentGame = GameData{
+        Joueur1:      j1,
+        Joueur2:      j2,
+        JetonCouleur: jetonCouleur,
+    }
+
+    // Redirection vers la page de jeu
+    http.Redirect(w, r, "/game/play", http.StatusSeeOther)
+})
+
 
 	http.HandleFunc("/game/play", func(w http.ResponseWriter, r *http.Request) {
-		err := listTemplate.ExecuteTemplate(w, "GamePlay", nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+		data := GamePage{
+			Grille: [6][7]string{{"", "", "", "", "", "", ""}, {"", "", "", "", "", "", ""}, {"", "", "", "", "", "", ""}, {"", "", "J", "R", "R", "", "J"}, {"", "R", "J", "R", "R", "", "R"}, {"J", "R", "J", "J", "J", "R", "J"}},
+			Tour:   1,
+			Joueur1: currentGame.Joueur1,
+			Joueur2: currentGame.Joueur2,
+			JetonCouleur: currentGame.JetonCouleur,
 		}
+
+		listTemplate.ExecuteTemplate(w, "GamePlay", data)
 	})
 
 	http.ListenAndServe("localhost:8000", nil)
